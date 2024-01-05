1d01210accf17c030def88cb58eeb7ed1e5bc373
Anna Kazulak <anna.kazulak@gmail.com>
2024-01-05 09:14:50 +0100

    delete unused server folder, add models for article and magazine

--- pages/api/welcome.ts
+++ pages/api/welcome.ts
@@ -1,11 +1,10 @@
 import mongoose, { Document, Schema } from 'mongoose';
 import type { NextApiRequest, NextApiResponse } from 'next'
-import Magazine from './magazine';
+import Magazine, { IMagazine } from './magazine';
 
 type ResponseData = {
-    message: any
+    message: IMagazine[] | null;
 }
-
 
 mongoose.connect(process.env.MONGODB_URI as string);
 mongoose.connection.on('connected', () => {
@@ -19,6 +18,39 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseData>
 ) {
-    const data = await Magazine.find({});
-    res.status(200).json({ message: data })
+    try {
+        const data = await Magazine.find({});
+        res.status(200).json({ message: data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: null });
+    }
 }
+
+
+
+
+// import mongoose, { Document, Schema } from 'mongoose';
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import Magazine from './magazine';
+
+// type ResponseData = {
+//     message: any
+// }
+
+
+// mongoose.connect(process.env.MONGODB_URI as string);
+// mongoose.connection.on('connected', () => {
+//     console.log('Connected to MongoDB');
+// })
+// mongoose.connection.on("error", (err) => {
+//     console.log(err);
+// });
+
+// export default async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse<ResponseData>
+// ) {
+//     const data = await Magazine.find({});
+//     res.status(200).json({ message: data })
+// }
